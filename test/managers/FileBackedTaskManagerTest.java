85c50af51774fe1b0e6b1a75c108eb02cee0ddc7
Viktoriia Novikova <koncord@yandex.ru>
2024-07-01 08:21:20 +0300

    Добавить проверку assertTrue collection.isEmpty() в классе FileBackedTaskManagerTest.

--- test/managers/FileBackedTaskManagerTest.java
+++ test/managers/FileBackedTaskManagerTest.java
@@ -43,9 +43,6 @@ class FileBackedTaskManagerTest extends TaskManagerTest {
         try {
             File file = File.createTempFile("test", "csv");
             FileBackedTaskManager fileManager = FileBackedTaskManager.loadFromFile(file);
-            assertEquals(fileManager.getAllSubTasks().size(), 0);
-            assertEquals(fileManager.getAllTasks().size(), 0);
-            assertEquals(fileManager.getAllEpics().size(), 0);
             assertTrue(fileManager.getAllTasks().isEmpty());
             assertTrue(fileManager.getAllEpics().isEmpty());
             assertTrue(fileManager.getAllSubTasks().isEmpty());
