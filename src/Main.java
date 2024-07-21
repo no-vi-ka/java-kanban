00445a2773fb412b58fdc0ab1ab1b11e37abb84a
Viktoriia Novikova <koncord@yandex.ru>
2024-07-21 04:55:40 +0300

    Исправить codeStyle.

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,13 @@
 import http.HttpTaskServer;
-import managers.InMemoryHistoryManager;
 import managers.InMemoryTaskManager;
-import tasks.Epic;
-import tasks.Status;
-import tasks.SubTask;
-import tasks.Task;
-
-import javax.sound.sampled.Port;
-import java.io.IOException;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.Month;
 
 public class Main {
 
     private static final InMemoryTaskManager taskManager = new InMemoryTaskManager();
     public static int PORT = 8080;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("Поехали!");
-
         HttpTaskServer httpTaskServer = new HttpTaskServer(PORT, taskManager);
         httpTaskServer.start();
     }
